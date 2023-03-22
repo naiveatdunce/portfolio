ad488ae156f1501c77602bd6367745d1e8005f07
Hafiz Husnain Raza <welcomehasnain@gmail.com>
2023-03-23 01:11:45 +0500

    Update app.js

--- app.js
+++ app.js
@@ -5,3 +5,9 @@ menuIcon.onclick= () =>{
     menuIcon.classList.toggle("bx-x");
     navbar.classList.toggle("active");
 };
+ document.addEventListener("click", (event) => {
+    if (!navbar.contains(event.target) && !menuIcon.contains(event.target) && navbar.classList.contains("active")) {
+      navbar.classList.remove("active");
+      menuIcon.classList.toggle("bx-x");
+    }
+  });
